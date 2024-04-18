c6ecdab219837067825812b47f7e5cf2f1513ec5
erick <etrujillov1@ucentral.edu.co>
2024-04-18 17:22:27 -0500

    primer commit

---
+++ src/test/java/ucentral/edu/co/appBanco/AppBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package ucentral.edu.co.appBanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
